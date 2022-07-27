0510b8b8e2e1f6d047a1b5b4b419e7ff21ef9a47
Ayed-Oukhay <oukhayayed@gmail.com>
2022-07-27 16:04:54 +0200

    working on the display NFT

--- frontend/src/views/pages/DisplayNFT.js
+++ frontend/src/views/pages/DisplayNFT.js
@@ -12,33 +12,35 @@ const NFT = (props) => {
 
     const [NFTs, setNFTs] = useState("");
     const [owner, setOwner] = useState("");
-    const [contractAddress, setContractAddress] = useState("0x7a1C29e5462989dB8680AaF5b9c1FeD6BDC16303");
+    const [contractAddress, setContractAddress] = useState("0x7a1C29e5462989dB8680AaF5b9c1FeD6BDC16303"); // For the moment the contract's address is hardcoded, seeing that all the NFTs created for the moments are done through the same smart contract.
     const [SpecNFT, setSpecNFT] = useState("");
 
-    //console.log(id);
+    // console.log(id);
 
-    // useEffect(async () => {
+    // ----------------------- Function to display the selected NFT using Alchemy's getNFTMetadata api -----------------------
+    // ----------------------- This api gets the metadata associated with a given NFT -----------------------
+    useEffect(async () => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
 
-    //     const addressArray = await window.ethereum.request({
-    //         method: "eth_accounts",
-    //     });
-    //     setOwner(addressArray[0]);
-    //     //console.log(addressArray[0]);
+        // --- Preparing the api's required variables ---
+        const baseURL = `${endpoint}/getNFTMetadata`;
+        const tokenId = "2";
+        const tokenType = "erc721";
+        const fetchURL = `${baseURL}?contractAddress=${contractAddress}&tokenId=${tokenId}&tokenType=${tokenType}`;
 
-    //     let data = await fetch(`${endpoint}/getNFTs?owner=${addressArray[0]}&contractAddresses%5B%5D=${contractAddress}`).then(data => data.json());
-    //     setNFTs(data.ownedNfts);
+        // --- Calling the api to get the metadata of the selected NFT ---
+        let data = await fetch(fetchURL, requestOptions)
+            .then(response => response.json())
+            .then(response => JSON.stringify(response, null, 2))
+            .then(result => console.log(result))
+            .then(result => setSpecNFT(result))
+            .catch(error => console.log('error', error));
 
-    //     let NFTList = data.ownedNfts;
-
-    //     for (let i = 0; i < NFTList.length; i++) {
-    //         if (NFTList[i].id === props.id) {
-    //             setSpecNFT(NFTList[i]);
-    //         }
-    //     }
-
-    //     console.log(SpecNFT);
-
-    // }, []);
+    }, [SpecNFT]);
+    // --------------------------------------------------------------------------------------------------------------------------
 
     return (
         <>
@@ -54,11 +56,10 @@ const NFT = (props) => {
                                 <div class="preview-pic tab-content" style={{ marginLeft: '80px', marginTop: '60px' }}>
                                     <div class="tab-pane active" id="pic-1"><img src="http://placekitten.com/400/252" /></div>
                                 </div>
-
                             </div>
                             <div class="details col-md-6">
                                 <br />
-                                <h3 class="product-title">Ayed's NFT</h3>
+                                <h3 class="product-title">{SpecNFT.title}</h3>
                                 <div class="rating">
                                     <div class="stars">
                                         <span class="fa fa-star checked"></span>
@@ -70,7 +71,7 @@ const NFT = (props) => {
                                     <span class="review-no">41 reviews</span>
                                 </div>
                                 <p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
-                                <h4 class="price">Current Price: &nbsp;&nbsp;<img src="https://cdn-icons-png.flaticon.com/512/7016/7016537.png" style={{height: '30px'}}/> 0.002
+                                <h4 class="price">Current Price: &nbsp;&nbsp;<img src="https://cdn-icons-png.flaticon.com/512/7016/7016537.png" style={{ height: '30px' }} /> 0.002
                                     &nbsp;&nbsp;<span>($180)</span></h4>
                                 <p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
                                 <h5 class="sizes">sizes:
@@ -86,7 +87,7 @@ const NFT = (props) => {
                                 </h5>
                                 <div class="action">
                                     <button class="add-to-cart btn btn-default" type="button"><i class="fas fa-credit-card" />&nbsp;&nbsp;&nbsp;Buy now</button>
-                                    <button class="like btn btn-default" type="button"><i class="fa fa-heart"/>&nbsp;&nbsp;&nbsp;Add to wishlist </button>
+                                    <button class="like btn btn-default" type="button"><i class="fa fa-heart" />&nbsp;&nbsp;&nbsp;Add to wishlist </button>
                                 </div>
                                 <br />
                             </div>
